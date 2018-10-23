5abd431de3e8154c54645025cff52190377ed6f1
Sara <sara.oliveira@pti.org.br>
2018-10-23 11:31:02 -0230

    Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { DiversaoComponent } from './diversao/diversao.component';
+import { ROUTES } from './app.routes';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    HomeComponent,
+    RodapeComponent,
+    RestaurantesComponent,
+    DiversaoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ofertas.service.ts
@@ -0,0 +1,22 @@
+import { Oferta } from "./shared/oferta.model";
+import { Http } from "@angular/http";
+import { Injectable } from "@angular/core";
+
+import 'rxjs/add/operator/toPromise'
+
+@Injectable()
+export class OfertasService {
+
+    constructor(private http: Http) { }
+
+
+
+    public getOfertas(): Promise<Oferta[]> {
+        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+            .toPromise()
+            .then((resposta: any) => resposta.json())
+    }
+
+
+
+}
